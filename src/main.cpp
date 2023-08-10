083bd2612e7cb5472bc75f8b7697a49a586cfecc
Hari <harkum@inventvm.com>
2023-08-10 11:19:18 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+  Serial.begin(9600); // open the serial port at 9600 bps:
+
+  // prints character
+  Serial.print("Welcome Hari,...........!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
